5afb874fecd63698fc09453e0c2447c584ee81ba
Mostafa-Esam1 <me2811075@gmail.com>
2025-11-23 21:08:08 +0200

    Crud system code

---
+++ main.js
@@ -0,0 +1,70 @@
+//Objects
+
+// let store = {
+//     nameStore: "om shima for tora7",
+//     branshes: [
+//       {
+//         //index 0
+//         location: "alex",
+//         manger: "Wael",
+//         Client: "20",
+//       },
+//       {
+//         //index 1
+//         location: "cairo",
+//         manger: "Ahmed",
+//         Client: "200",
+//       },
+//     ],
+
+// };
+//       console.log(store.branshes);
+
+// let Dipslay2 = () => {
+//   store = {
+//     loctaion: prompt("Enter the Location"),
+//     Manger: prompt("Enter the Name"),
+//     client: prompt("Enter the Cleint"),
+//   };
+//   console.log(store.client);
+// };
+
+// let EditUsers = () => {
+//   console.clear();
+//   let NewName = +prompt("Enter new cleint");
+//   store.client = NewName;
+//   console.log(store);
+// };
+
+//Arraw of objects
+
+let phones = [
+  { name: "iphone x", price: 200, qty: 10 },
+  { name: "iphone 11", price: 400, qty: 6 },
+  { name: "iphone 12", price: 600, qty: 16 },
+];
+
+let ShowPhones = () => {
+  console.clear();
+  console.table(phones);
+};
+
+let addPhones = () => {
+  let NewPhone = {
+    name: prompt("Add new phone"),
+    price: +prompt("Add new price"),
+    qty: +prompt("Add  qty"),
+  };
+  phones.push(NewPhone);
+  ShowPhones();
+};
+
+let DeletePhone = () => {
+  let IndexDelete = +prompt("enter index for delete");
+  phones.splice(IndexDelete, 1);
+  ShowPhones();
+};
+
+let Update = () => {
+    
+}
